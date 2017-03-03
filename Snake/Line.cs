d4c64108296277d5b62ecdaf0615241a602c7d4a
profession <x-profession@yandex.ru>
2017-03-03 15:57:31 +0200

    Method which draw borders

---
+++ Snake/Line.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Snake
+{
+	public class Line
+	{
+		int x;
+		int y;
+		int lenght;
+		char symb;
+		LineDirection direction;
+
+		public Line(int _x, int _y, int _lenght, LineDirection _direction, char _symb)
+		{
+			x = _x;
+			y = _y;
+			lenght = _lenght;
+			direction = _direction;
+			symb = _symb;
+			Draw();
+		}
+
+		public void Draw()
+		{
+			Console.SetCursorPosition(x, y);
+			if (direction == LineDirection.Horizontal)
+			{
+				for (int i = 0; i < lenght; i++)
+				{
+					Console.Write(symb);
+				}
+			}
+			else if (direction == LineDirection.Vertical)
+			{
+				for (int i = 0; i < lenght; i++)
+				{
+					Console.SetCursorPosition(x, y + i);
+					Console.Write(symb);
+				}
+			}
+		}
+
+		public enum LineDirection
+		{
+			Horizontal,
+			Vertical
+		}
+	}
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Snake
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Line topLine = new Line(1, 0, 78, Line.LineDirection.Horizontal, '-');
+			Line bottomLine = new Line(1, 24, 78, Line.LineDirection.Horizontal, '-');
+			Line leftLine = new Line(0, 1, 23, Line.LineDirection.Vertical, '|');
+			Line RightLine = new Line(79, 1, 23, Line.LineDirection.Vertical, '|'); 
+			Console.ReadKey();
+		}
+	}
+}
